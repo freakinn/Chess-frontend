f5ba45cc7cd9e6b2d07055eb8572140b3f7bfaee
freakinn <rahuldhandoreeas21b0094@gmail.com>
2026-05-15 22:27:23 +0530

    updated backend url

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState, useRef } from 'react';
 import { Chess } from 'chess.js';
 import { Chessboard } from 'react-chessboard';
 import { io } from 'socket.io-client';
-const socket = io("http://localhost:3000");
+const socket = io("https://chess-backend-theta.vercel.app/");
 
 const App = () => {
 
